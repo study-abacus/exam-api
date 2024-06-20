ae010fbfd306c827a7e0c4b75559f2a8c242fe6f
Prerna Singh <prernasingh@Prernas-MacBook-Pro.local>
2024-06-20 22:51:47 +0530

    Admit card Model

--- api/server/app/models/admit_card.py
+++ api/server/app/models/admit_card.py
@@ -1,18 +1,16 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean ,  Float, Text
+from sqlalchemy import Column, Integer, String, Text, Sequence
 from sqlalchemy.dialects.postgresql import ARRAY
-
-from typing import Any
-
 from app.db.base_class import Base
 
+admit_card_id_seq = Sequence('admit_card_id_seq', start=1000, increment=1)
+
 class AdmitCard(Base):
-    id = Column(Integer, primary_key = True, nullable = True)
-    order_id = Column(Text)
-    examination_ids = Column(ARRAY(Integer))
-    password_hash = Column(String(100))
+    id = Column(Integer, admit_card_id_seq, primary_key=True, nullable=False, server_default=admit_card_id_seq.next_value())
+    order_id = Column(Text, nullable=True)
+    examination_ids = Column(ARRAY(Integer), nullable=True)
+    password_hash = Column(String(100), nullable=True)
     profile_id = Base.foreign_key(Integer, "id", "Profile")
     championship_id = Base.foreign_key(Integer, "id", "Championship")
-
 
     def __init__(self, **kwargs) -> None: 
         super().__dict__.update(kwargs)
