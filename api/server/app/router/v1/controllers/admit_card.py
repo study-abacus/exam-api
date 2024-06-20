67a91fd742014908b823ecb7afc53bc289e980df
Prerna Singh <writeprernasingh@gmail.com>
2024-06-20 11:26:35 +0530

    Update admit_card.py

--- api/server/app/router/v1/controllers/admit_card.py
+++ api/server/app/router/v1/controllers/admit_card.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
-@router.post("/authenticate")
+@router.post("/authenticate/")
 async def authenticate_admit_card(admit_card: AdmitCardAuthenticateBase, db: Session = Depends(deps.get_session)):
     """
     Authenticate admit_card.
@@ -24,7 +24,7 @@ async def authenticate_admit_card(admit_card: AdmitCardAuthenticateBase, db: Ses
     return handle_result(result)
 
 
-@router.put("/current", response_model=Profile)
+@router.put("/current/", response_model=Profile)
 async def create_current_admit_card(profile:ProfileUpdate,admit_card:dict = Depends(deps.get_admit_card), db: Session = Depends(deps.get_session)):
     """
     Create new admit_card.
@@ -32,93 +32,10 @@ async def create_current_admit_card(profile:ProfileUpdate,admit_card:dict = Depe
     result = await AdmitCardService(db).update_current_admit_card(profile,admit_card)
     return handle_result(result)
 
-@router.get("/current", response_model=Profile)
+@router.get("/current/", response_model=Profile)
 async def create_current_admit_card(admit_card:dict = Depends(deps.get_admit_card), db: Session = Depends(deps.get_session)):
     """
     Create new admit_card.
     """
     result = await AdmitCardService(db).get_current_admit_card(admit_card)
     return handle_result(result)
-
-
-@router.get("/", response_model=List[AdmitCard])
-async def read_admit_cards(skip: int = 0, limit: int = 100, db: Session = Depends(deps.get_session)):
-    """
-    Retrieve admit_cards.
-    """
-    result = await AdmitCardService(db).get_admit_cards( skip=skip, limit=limit)
-    return handle_result(result)
-
-@router.post("/{profile_id}", response_model=AdmitCard)
-async def create_admit_card(profile_id: int, admit_card: AdmitCardCreateManual, championship_id: int= Depends(deps.championship_id_param), examination_ids : List[int] = Depends(deps.examination_ids_param), db: Session = Depends(deps.get_session)):
-    """
-    Create new admit_card.
-    """
-    result = await AdmitCardService(db).create_admit_card(profile_id, championship_id, examination_ids, admit_card)
-    return handle_result(result)
-
-@router.get("/{admit_card_id}", response_model=AdmitCard)
-async def read_admit_card(admit_card_id: int, db: Session = Depends(deps.get_session)):
-    """
-    Retrieve admit_card.
-    """
-    result = await AdmitCardService(db).get_admit_card(admit_card_id)
-    return handle_result(result)
-
-@router.put("/{admit_card_id}", response_model=AdmitCard)
-async def update_admit_card(admit_card_id: int, admit_card: AdmitCardUpdate,championship_id: int= Depends(deps.championship_id_param), examination_ids : List[int] = Depends(deps.examination_ids_param), db: Session = Depends(deps.get_session)):
-    """
-    Update admit_card.
-    """
-    result = await AdmitCardService(db).update_admit_card(admit_card_id, admit_card, championship_id, examination_ids)
-    return handle_result(result)
-
-@router.delete("/{admit_card_id}")
-async def delete_admit_card(admit_card_id: int, db: Session = Depends(deps.get_session)):
-    """
-    Delete admit_card.
-    """
-    result = await AdmitCardService(db).delete_admit_card(admit_card_id)
-    return handle_result(result)
-
-
-@router.get("/profiles/{profile_id}/admit_cards", response_model=List[AdmitCard])
-async def read_profile_admit_cards(profile_id: int, db: Session = Depends(deps.get_session)):
-    """
-    Retrieve admit_cards for profile.
-    """
-    result = await AdmitCardService(db).get_profile_admit_cards(profile_id)
-    return handle_result(result)
-
-@router.get("/championships/{championship_id}/admit_cards", response_model=List[AdmitCard])
-async def read_championship_admit_cards(championship_id: int, db: Session = Depends(deps.get_session)):
-    """
-    Retrieve admit_cards for championship.
-    """
-    result = await AdmitCardService(db).get_championship_admit_cards(championship_id)
-    return handle_result(result)
-
-@router.get("/examinations/{examination_id}/admit_cards", response_model=List[AdmitCard])
-async def read_examination_admit_cards(examination_id: int, db: Session = Depends(deps.get_session)):
-    """
-    Retrieve admit_cards for examination.
-    """
-    result = await AdmitCardService(db).get_examination_admit_cards(examination_id)
-    return handle_result(result)
-
-@router.get("/{profile_id}/championship/{championship_id}/admit_cards", response_model=List[AdmitCard])
-async def read_profile_championship_admit_cards(profile_id: int, championship_id: int, db: Session = Depends(deps.get_session)):
-    """
-    Retrieve admit_cards for profile and championship.
-    """
-    result = await AdmitCardService(db).get_profile_championship_admit_cards(profile_id, championship_id)
-    return handle_result(result)
-
-@router.get("/{profile_id}/examination/{examination_id}/admit_cards", response_model=List[AdmitCard])
-async def read_profile_examination_admit_cards(profile_id: int, examination_id: int, db: Session = Depends(deps.get_session)):
-    """
-    Retrieve admit_cards for profile and examination.
-    """
-    result = await AdmitCardService(db).get_profile_examination_admit_cards(profile_id, examination_id)
-    return handle_result(result)
-
