5d0052dce15c12c4f7ed1807bffbeec796d20cf0
Prerna Singh <writeprernasingh@gmail.com>
2024-06-20 11:02:42 +0530

    Update admit_card.py

--- api/server/app/router/v1/controllers/admit_card.py
+++ api/server/app/router/v1/controllers/admit_card.py
@@ -2,11 +2,11 @@ from fastapi import APIRouter, Depends, HTTPException
 from typing import List, Any , Optional, Union
 from sqlalchemy.orm import Session
 
-from app.utils.service_request import handle_result
-from app.utils.jwt import decode_jwt_token
-from app.schemas.admit_card import AdmitCardAuthenticateBase,AdmitCardAuthenticate, AdmitCardCreate, AdmitCard, AdmitCardUpdate, AdmitCardCreateManual
-from app.schemas.profile import ProfileUpdate, Profile
-from app.services.admit_card import AdmitCardService
+from utils.service_request import handle_result
+from utils.jwt import decode_jwt_token
+from schemas.admit_card import AdmitCardAuthenticateBase,AdmitCardAuthenticate, AdmitCardCreate, AdmitCard, AdmitCardUpdate, AdmitCardCreateManual
+from schemas.profile import ProfileUpdate, Profile
+from services.admit_card import AdmitCardService
 
 from app.router import deps
 import logging
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
-@router.post("/authenticate/")
+@router.post("/authenticate")
 async def authenticate_admit_card(admit_card: AdmitCardAuthenticateBase, db: Session = Depends(deps.get_session)):
     """
     Authenticate admit_card.
@@ -24,13 +24,22 @@ async def authenticate_admit_card(admit_card: AdmitCardAuthenticateBase, db: Ses
     return handle_result(result)
 
 
-@router.put("/current/", response_model=Profile)
+@router.put("/current", response_model=Profile)
 async def create_current_admit_card(profile:ProfileUpdate,admit_card:dict = Depends(deps.get_admit_card), db: Session = Depends(deps.get_session)):
     """
     Create new admit_card.
     """
     result = await AdmitCardService(db).update_current_admit_card(profile,admit_card)
     return handle_result(result)
+
+@router.get("/current", response_model=Profile)
+async def create_current_admit_card(admit_card:dict = Depends(deps.get_admit_card), db: Session = Depends(deps.get_session)):
+    """
+    Create new admit_card.
+    """
+    result = await AdmitCardService(db).get_current_admit_card(admit_card)
+    return handle_result(result)
+
 
 @router.get("/", response_model=List[AdmitCard])
 async def read_admit_cards(skip: int = 0, limit: int = 100, db: Session = Depends(deps.get_session)):
