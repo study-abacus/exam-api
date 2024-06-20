65c9bf9c856c8ed6be6626aef1fb380df0fedc61
Prerna Singh <prernasingh@Prernas-MacBook-Pro.local>
2024-06-20 20:08:20 +0530

    Profile changes

--- api/server/app/schemas/profile.py
+++ api/server/app/schemas/profile.py
@@ -6,9 +6,14 @@ from datetime import datetime
 class ProfileBase(BaseModel):
     name: Optional[str]
     ci : Optional[str]
+    sa_class : Optional[str]
+    city : Optional[str]
+    country : Optional[str]
+    age : Optional[str]
     guardian_name: Optional[str]
     email: Optional[str]
     phone: Optional[str]
+
 
 
 
